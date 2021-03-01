944c38854a8230268158027c617cfdfe1da6d9a7
viktak <github@viktak.com>
2021-03-01 11:48:06 +0200

    build number removed

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "179"
+  #define BUILD_NUMBER "183"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.0.179 - 2021-03-01 11:39:23.556533"
+  #define FIRMWARE_VERSION "v1.0.183 - 2021-03-01 11:47:21.634819"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.0.179"
+  #define FIRMWARE_VERSION_SHORT "v1.0.183"
 #endif
