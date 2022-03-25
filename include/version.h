61f0ae6e8997ec3993cf37d19d11f7b8ccafafd5
viktak <github@viktak.com>
2022-03-25 11:29:16 +0200

    better NTP management

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "114"
+  #define BUILD_NUMBER "137"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.1.114 - 2022-03-16 17:07:06.299626"
+  #define FIRMWARE_VERSION "v1.1.137 - 2022-03-25 11:26:43.216070"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.1.114"
+  #define FIRMWARE_VERSION_SHORT "v1.1.137"
 #endif
